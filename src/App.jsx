ccc081bafc01870f1d2a2e2476324fb19247fd73
Eyoel Tewodros <eyoeltewodros93@gmail.com>
2022-11-22 18:32:04 +0300

    final project

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from "./components/contact/Contact";
 import "./app.scss";
 import { useState } from "react";
 import Menu from "./components/menu/Menu";
-//
+
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
