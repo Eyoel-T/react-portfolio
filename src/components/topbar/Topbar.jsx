dd3a4785cb762b2be82fdbe13da9e91c2a87fde3
Eyoel Tewodros <eyoeltewodros93@gmail.com>
2022-11-22 18:30:14 +0300

    navbar added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,6 +2,7 @@ import "./topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
+  //
   return (
     <div className={menuOpen ? "topbar active" : "topbar"}>
       <div className="wrapper">
